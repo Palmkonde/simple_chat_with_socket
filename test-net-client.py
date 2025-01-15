c57b5e381531e9dfead42490dc498ff4cd7fb927
PalmPalmPalmPalmPalm <93920339+Palmkonde@users.noreply.github.com>
2025-01-15 21:01:21 +0700

    adding some useful function but forgot what is inside function

--- test-net-client.py
+++ test-net-client.py
@@ -1,46 +1,54 @@
 import socket
 import threading
 
-HOST = '10.237.28.221'
-PORT = 21001
+HOST = '127.0.0.1'
+PORT = 50005
 
 connecting_status = True
 
-def send_message_function(client_socket):
+def send_message(server_socket: socket.socket) -> None:
     global connecting_status
     while True:
         message = input("Enter a message: ")
-        client_socket.send((message + "\n").encode())
+        server_socket.send((message + "\n").encode())
         
         if message.lower() == "exit":
-            client_socket.close()
+            server_socket.shutdown(socket.SHUT_RDWR) 
+            server_socket.close()
             connecting_status = False
             break
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.connect((HOST, PORT))
     print("Connected to server")
 
-    send_thread = threading.Thread(target=send_message_function, args=(s,))
+    send_thread = threading.Thread(target=send_message, args=(sock,))
     send_thread.start()
 
-    while connecting_status:
-        message_received = ""
-        while True:
-            data = s.recv(1024)
-            if data:
-                print('Received data chunk from server: ', repr(data))
-                message_received += data.decode()
-                if message_received.endswith("\n"):
-                    print("End of message received")
+    try:
+        # receiving messages
+        while connecting_status:
+            message_received = ""
+            while True:
+                data = sock.recv(1024)
+                if data:
+                    print('Received data chunk from server: ', repr(data))
+                    message_received += data.decode()
+                    if message_received.endswith("\n"):
+                        print("End of message received")
+                        break
+                else:
+                    print("Connection lost!")
+                    connecting_status = False
                     break
-            else:
-                print("Connection lost!")
-                connecting_status = False
-                break
-        if not message_received:
-            break
 
-        print("Server: ", message_received)
+            print("Server: ", message_received)
+
+    except (ConnectionAbortedError, OSError):
+        print("Socket Closed")
+    
+    finally:
+        connecting_status = False
+        print("Existing Client...")
 
 print("Client finished")
--- test-server.py
+++ test-server.py
@@ -3,55 +3,53 @@ import threading
 
 HOST = '0.0.0.0'
 PORT = 50005
-s = None
+server = None
+client = []
 
-def send_message_function(client_socket):
+def send_message_function(client_socket: socket.socket) -> None:
+    """ Using for sending messages to client """
     while True:
         message = input("Enter a message: ")
         client_socket.send((message + "\n").encode())
 
+def broad_cast(message: str,
+               sender_socket: socket.socket) -> None:
+    """ Sending messages from one to many clients"""
+    pass
+
+def handle_client(client_socket: socket.socket):
+    """ Handle messages for each client """
+    while True:
+        data = client_socket.recv(1024)
+
+
+# Start initialize server socket
 try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("Socket created")
 except OSError as msg:
-    s = None
+    server = None
     print(f"Error creating socket: {msg}")
     exit(1)
 
 try:
-    s.bind((HOST, PORT))
-    s.listen()
-    print("Socket bound and listening")
+    server.bind((HOST, PORT))
+    server.listen()
+    print(f"Socket bound and server is listening on {HOST}:{PORT}")
 except OSError as msg:
     print("Error binding/listening!")
-    s.close()
+    server.close()
     exit(1)
+# end initial server socket
 
-conn, addr = s.accept()
-with conn:
-    print('Connection accepted from ', addr)
+# main loop for handle client and others
+while True:
+    client_socket, client_address = server.accept()
+    with client_socket:
+        print('Connection accepted from ', client_address)
 
-    send_thread = threading.Thread(target=send_message_function, args=(conn, ))
-    send_thread.start()
+        send_thread = threading.Thread(target=send_message_function, args=(client_socket, ))
+        send_thread.start()
 
-    while True:
-        message_received = ""
-        while True:
-            data = conn.recv(1024)
-            if data:
-                print('Received data chunk from client: ', repr(data))
-                message_received += data.decode()
-                if message_received.endswith("\n"):
-                    break
-            else:
-                print("\nConnection lost!")
-                break
-
-        if message_received:
-            print("Client: ", message_received)
-        else:
-            print("Waiting for new client...")
-            break
-
-s.close()
+server.close()
 print("Server finished")
